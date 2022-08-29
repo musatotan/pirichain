b142aec1d9db406b4450247650edd3162fdae41d
musatotan <musatotan@gmail.com>
2022-08-30 02:23:00 +0300

    createToken.js is updated with FormData.

--- src/token/createToken.js
+++ src/token/createToken.js
@@ -24,26 +24,35 @@ module.exports.createToken = (
     sector = "",
     hasAirdrop = false,
     hasStake = false
-) => client.post(endpoint, {
-    creatorAddress : creatorAddress,
-    privateKey : privateKey,
-    tokenName : tokenName,
-    tokenSymbol : tokenSymbol,
-    totalSupply : totalSupply,
-    logo : logo,
-    decimals : decimals,
-    description : description,
-    webSite : webSite,
-    socialMediaFacebookURL : socialMediaFacebookURL,
-    socialMediaTwitterURL : socialMediaTwitterURL,
-    socialMediaMediumURL : socialMediaMediumURL,
-    socialMediaYoutubeURL : socialMediaYoutubeURL,
-    socialMediaRedditURL : socialMediaRedditURL,
-    socialMediaBitcoinTalkURL : socialMediaBitcoinTalkURL,
-    socialMediaInstagramURL : socialMediaInstagramURL,
-    mailAddress : mailAddress,
-    companyAddress : companyAddress,
-    sector : sector,
-    hasAirdrop : hasAirdrop,
-    hasStake : hasStake
-});
+) => {
+    const form = new FormData();
+    form.append('creatorAddress', creatorAddress);
+    form.append('privateKey', privateKey);
+    form.append('tokenName', tokenName);
+    form.append('tokenSymbol', tokenSymbol);
+    form.append('totalSupply', totalSupply);
+    form.append('logo', logo);
+    form.append('decimals', decimals);
+    form.append('description', description);
+    form.append('webSite', webSite);
+    form.append('socialMediaFacebookURL', socialMediaFacebookURL);
+    form.append('socialMediaTwitterURL', socialMediaTwitterURL);
+    form.append('socialMediaMediumURL', socialMediaMediumURL);
+    form.append('socialMediaYoutubeURL', socialMediaYoutubeURL);
+    form.append('socialMediaRedditURL', socialMediaRedditURL);
+    form.append('socialMediaBitcoinTalkURL', socialMediaBitcoinTalkURL);
+    form.append('socialMediaInstagramURL', socialMediaInstagramURL);
+    form.append('mailAddress', mailAddress);
+    form.append('companyAddress', companyAddress);
+    form.append('sector', sector);
+    form.append('hasAirdrop', hasAirdrop);
+    form.append('hasStake', hasStake);
+
+
+    return client.post(endpoint, form, {
+        headers:{
+            'Content-Type': 'multipart/form-data'
+        }
+    })
+
+};
